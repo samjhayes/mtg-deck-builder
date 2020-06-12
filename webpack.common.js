09545422612e16379e7e17a60001a699d35b3615
Sam Hayes <sam.hayes@gamer-network.net>
2020-06-12 13:41:05 +0100

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.js",
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html"
+    }),
+    new VueLoaderPlugin()
+  ]
+};
